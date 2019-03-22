106f37c09b667b8924d2b1f745aa3dd50a20d32d
zarinaomor <zarinaomorova@yahoo.com>
2019-03-21 21:28:31 -0700

    Commit 2 - Palindrome.

---
+++ js/app.js
@@ -0,0 +1,45 @@
+// What is the difference between a parameter and an argument?
+    // parameters are passed when function is declared
+    // arguments are passed when function invokes
+
+// Within a function, what is the difference between return and console.log?
+    // return is output that was given to function. It prints the value.
+    // console.log just prints the message on console and returns undefined.
+
+// What are the implications of the ability of a function to return a value?
+    // to give actual value to the putput of the function
+
+// 2. Palindrome again.
+// Write a function checkPalindrome that accepts a single argument, a string. 
+// The function should return true if the string is a palindrome, false if not. 
+// Make sure your function will give the correct answer for words with capital letters.
+
+// GO BACK!!!!!!!!!!!!!!!
+ 
+const checkPalindrome = (str) => {
+    if(str.split(' ').reverse().join()) {
+        // return true;
+    }else {
+        // return false;
+    }
+
+
+}
+// console.log(checkPalindrome("Zarina"));
+
+// 3. Digit Sum
+// Write a function sumDigits that accepts a number and returns the sum of its digits.
+
+// GO BACK!!!!!!!!!!!!!!!
+
+const sumDigits = (num) => {
+    let sum = 0;
+    let numToString = num.toString().split(' ');
+    for (let i=1; i<numtToString.length; i++) {
+        sum += Number(numToString[i]);
+    }
+    // return sum;    
+
+}
+// console.log(sumDigits(42)); //6
+
